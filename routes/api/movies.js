dd639946315d48379a30040295904a3412813189
capetillo <carolinacapetilloc@gmail.com>
2020-01-20 16:39:56 -0800

    makes a RESTful route (corrects a mistake)

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const moviesCtrl = require("../../controllers/movies");
 
-router.get("/movies", moviesCtrl.getAllMovies);
+router.get("/", moviesCtrl.getAllMovies);
 
 module.exports = router;
