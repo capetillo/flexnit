18c2d3272cacf6058aaf69d4a2e66a2a9e9a6b90
capetillo <carolinacapetilloc@gmail.com>
2020-01-22 14:57:00 -0800

    changes size of image

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,11 +2,13 @@ import React, { useState, Component } from "react";
 import Results from "../Results/Results";
 import movieService from "../../utils/movieService";
 //import SaveButton from "./SaveButton/SaveButton";
+import "./Movies.css";
 
 class Movies extends Component {
   state = {
     title: "",
     streaming: [],
+    imgArr: [],
     available: "",
     searchArr: [],
     searchRedirect: false
@@ -32,24 +34,29 @@ class Movies extends Component {
   checkStream = () => {
     console.log("check stream");
     let tempArray = [];
+    let imgArray = [];
     movieService
       .checkStream(this.state.title)
       .then(response => {
-        console.log(response);
         console.log("RESPONSE DATA IS ", response.data);
         if (response.data.length > 0) {
+          imgArray.push(response.data[0].picture);
+          console.log("TEMP ARRAY", tempArray);
           for (let i = 0; i < response.data[0].locations.length; i++) {
-            console.log(response.data[0].locations[i].display_name);
             tempArray.push(response.data[0].locations[i].display_name);
-            tempArray.push(response.data[0].picture);
           }
         }
       })
       .then(() => {
         if (tempArray.length > 0) {
-          this.setState({ streaming: tempArray, available: "Available On: " });
+          this.setState({
+            streaming: tempArray,
+            imgArr: imgArray,
+            available: "Available On: "
+          });
         } else {
           this.setState({
+            imgArr: imgArray,
             streaming: tempArray,
             available:
               "Womp womp, this isn't currently available on any streaming platform."
@@ -81,7 +88,7 @@ class Movies extends Component {
                 <p key={service}>{service}</p>
               ))}
             </div>
-            <img src={this.state.streaming[1]} alt="u suck" />
+            <img src={this.state.imgArr} className="img" />
           </div>
         </form>
         {/* <SaveButton onClick={this.props.handleDeleteMovie} /> */}
