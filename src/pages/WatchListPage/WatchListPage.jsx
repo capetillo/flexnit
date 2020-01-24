e195c65d3dfc88830ec4d7175076ba769ffe4bca
capetillo <carolinacapetilloc@gmail.com>
2020-01-23 21:39:21 -0800

    fixes button issue on watchlist

--- src/pages/WatchListPage/WatchListPage.jsx
+++ src/pages/WatchListPage/WatchListPage.jsx
@@ -3,34 +3,30 @@ import movieService from "../../utils/movieService";
 import Nav from "../../components/Nav/Nav";
 import "./WatchListPage.css";
 
-const WatchListPage = props => {
-  let watchlistpage = (
-    <div>
-      <Nav user={props.user} />
-      {props.user.watchlist.map(movie => (
-        <div>
-          {movie.name}
-          <img className="img" src={movie.picture} alt=""></img>
-          <button
-            onClick={() =>
-              movieService
-                .deleteFromList(props.user._id, movie.name)
-                .then(res => {
-                  console.log("RESSSS ", res);
-                  let newUser = res.data;
-                  props.handleUpdateUser(newUser);
-                })
-            }
-          >
-            {" "}
-            -{" "}
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-
-  return <div className="WatchlistPage">{watchlistpage}</div>;
-};
+const WatchListPage = props => (
+  <div>
+    <Nav user={props.user} />
+    {props.user.watchlist.map(movie => (
+      <div>
+        {movie.name}
+        <img className="img" src={movie.picture} alt=""></img>
+        <button
+          onClick={() =>
+            movieService
+              .deleteFromList(props.user._id, movie.name)
+              .then(res => {
+                console.log("RESSSS ", res);
+                let newUser = res.data;
+                props.handleUpdateUser(newUser);
+              })
+          }
+        >
+          {" "}
+          -{" "}
+        </button>
+      </div>
+    ))}
+  </div>
+);
 
 export default WatchListPage;
